af1e768f859a7ce2917c4fb3c0b2a7e8378afc42
Kevin Conner <kconner@redhat.com>
2019-04-26 15:56:42 -0700

    Remove unused scripts

--- scripts/cleanup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-oc cluster down
-mount | grep openshift | awk '{print $3}' | xargs umount
-rm -rf /var/lib/origin/*
--- scripts/connect.sh
+++
@@ -1,7 +0,0 @@
-if [ ! -z "$2" ]
-  then
-    export IDENTITY="-i $2"
-fi
-
-export LC_MYIP=`dig $1 +short`
-ssh -o SendEnv=LC_MYIP $IDENTITY lab-user@$1
