9d08a42c03e37e2ccadc5c1c35a8b3e75c7c37d2
Kevin Conner <kconner@redhat.com>
2019-04-25 18:58:23 -0700

    Update source to quarkus

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -3,18 +3,12 @@
 MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 MYHOME=${MYDIR}/..
 
-RECO_SRC=${MYHOME}/src/recommendation/src/main/java/com/redhat/developer/demos/recommendation/RecommendationVerticle.java
-
-TAG_PREFIX=istio-lab
+TAG_PREFIX=example
 
 function fail() {
-	git checkout -f ${RECO_SRC}
 	echo $1
 	exit 1
 }
-
-# reset recommendation state
-git checkout -f ${RECO_SRC}
 
 for project in customer preference recommendation ; do
 	mvn -f ${MYHOME}/src/${project} clean package -DskipTests && \
@@ -22,20 +16,7 @@ for project in customer preference recommendation ; do
 		fail "build of $project failed"
 done
 
-# recommendation:v2
-sed -i.bak 's/recommendation v. from/recommendation v2 from/' ${RECO_SRC}
-
-mvn -f ${MYHOME}/src/recommendation clean package -DskipTests && \
-	docker build -t ${TAG_PREFIX}/recommendation:v2 ${MYHOME}/src/recommendation || \
-	fail "build of recommendation:v2 failed"
-
-# recommendation:v2d (with delay)
-sed -i.bak 's|^//.*this::timeout.*$|router.get("/").handler(this::timeout);|' ${RECO_SRC}
-
-mvn -f ${MYHOME}/src/recommendation clean package -DskipTests && \
-	docker build -t ${TAG_PREFIX}/recommendation:v2d ${MYHOME}/src/recommendation || \
-	fail "build of recommendation:v2d failed"
-
-# reset file
-git checkout -f ${RECO_SRC}
-
+for version in v2 v3 ; do
+	docker build -t ${TAG_PREFIX}/${project}:${version} ${MYHOME}/src/${project} || \
+	fail "build of $project failed"
+done
